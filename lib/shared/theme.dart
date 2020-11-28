af9d8a6dd2f08ceb5cb19384a6ce3ddb8d7ba334
lanaysp <lanaysps@gmail.com>
2020-11-29 03:53:49 +0700

    add general page,signin page,sign up page

---
+++ lib/shared/theme.dart
@@ -0,0 +1,15 @@
+part of 'shared.dart';
+
+Color greyColor = "8D92A3".toColor();
+Color mainColor = "FFC700".toColor();
+
+TextStyle greyFontStyle = GoogleFonts.poppins().copyWith(color: greyColor);
+TextStyle blacFontStyle = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 22, fontWeight: FontWeight.w500);
+TextStyle blacFontStyle2 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black, fontSize: 16, fontWeight: FontWeight.w500);
+TextStyle blacFontStyle3 = GoogleFonts.poppins()
+    .copyWith(color: Colors.black,);
+    
+
+const double defaultMargin = 24;
